62e6378d28240bf222dd48f7dbd24881972ac068
gizeasy <gizeasy@gmail.com>
2022-01-17 18:39:57 +0300

    feat(Layout): add props table

--- src/Layout/types.ts
+++ src/Layout/types.ts
@@ -1,13 +1,13 @@
 import { PropsWithHTMLAttributesAndRef } from '@/__private__/utils/types/PropsWithHTMLAttributes'
 
+export type LayoutSlotHeight = 'm' | 's'
+
 export type LayoutRow = {
   left: React.ReactNode
   right: React.ReactNode
   center: React.ReactNode
   height?: LayoutSlotHeight
 }
-
-export type LayoutSlotHeight = 'm' | 's'
 
 export type LayoutPropRowCenter = React.ReactNode | LayoutRow
 export type LayoutPropRowTop = React.ReactNode | Omit<LayoutRow, 'height'>
