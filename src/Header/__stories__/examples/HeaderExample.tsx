abf2ebb21f8c9dcba17d03b557c5f8ed0cf41baf
Anya Ovsyannikova <arhayka@gmail.com>
2022-01-19 15:30:37 +0300

    docs(Header): edit docs
    closes #1901

---
+++ src/Header/__stories__/examples/HeaderExample.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+import { Example } from '@/__private__/storybook'
+
+import { ConstaLogo } from '../ConstaLogo'
+import { menu, notifications, notificationsActions, tileMenu } from '../../__mocks__/data.mock'
+
+import { Header } from '@/Header'
+
+export const HeaderExampleBasic = () => (
+  <Example>
+    <Header
+      logo={<ConstaLogo />}
+      userName="Иванов Иван"
+      userInfo="Владелец"
+      loginButtonLabel="Войти"
+      userLogined={true}
+      notifications={notifications}
+      notificationsActions={notificationsActions}
+      notificationsTitle="Уведомления"
+      notificationsGroupByDay={true}
+      tileMenu={tileMenu}
+      tileMenuTitle="Сервисы"
+      menu={menu}
+    />
+  </Example>
+)
