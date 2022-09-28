26bb907b193c82f15b2fe083decbbf109e745f8d
gizeasy <gizeasy@gmail.com>
2022-09-28 12:39:39 +0300

    dd

--- src/stand/standConfig.ts
+++ src/stand/standConfig.ts
@@ -2,17 +2,16 @@ import { createConfig } from '@consta/stand';
 
 import image from './ConstaImage.png';
 
-const groups = [
-  {
-    title: 'Компоненты',
-    id: 'components',
-  },
-] as const;
-
 export const { createStand } = createConfig({
   title: 'Consta Header',
   id: 'header',
-  groups,
+  groups: [
+    {
+      title: 'Компоненты',
+      id: 'components',
+      view: 'card',
+    },
+  ],
   group: 'Библиотеки компонентов',
   image,
   description: 'Библиотека компонентов, из которых собирается шапка проекта.',
