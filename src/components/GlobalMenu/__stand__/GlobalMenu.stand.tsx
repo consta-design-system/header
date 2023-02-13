d9718259045aa39d5e8db2de47e973f301733afe
Maxim Kolesnikov <socialfox33@gmail.com>
2023-02-13 18:14:32 +0300

    docs(sandbox): add links to sandbox for new components
    closes consta-design-system/uikit#2856

--- src/components/GlobalMenu/__stand__/GlobalMenu.stand.tsx
+++ src/components/GlobalMenu/__stand__/GlobalMenu.stand.tsx
@@ -7,6 +7,7 @@ export default createStand({
   description: 'Глобальное меню с группами',
   version: '1.0.2',
   status: 'canary',
+  sandbox: 'header-globalmenu-eozui5',
   figma:
     'https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FLKYyjgQ3khFHvXraZAG1n1%2FConsta-Components%3Fnode-id%3D1406%253A79843%26t%3DENHeeeYRjfcwnuCR-1',
 });
--- src/components/NavBar/__stand__/NavBar.stand.tsx
+++ src/components/NavBar/__stand__/NavBar.stand.tsx
@@ -7,6 +7,7 @@ export default createStand({
   description: 'NavBar',
   version: '1.0.2',
   status: 'canary',
+  sandbox: 'header-navbar-ut51f0',
   figma:
     'https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FLKYyjgQ3khFHvXraZAG1n1%2FConsta-Components%3Fnode-id%3D1406%253A79843%26t%3DENHeeeYRjfcwnuCR-1',
 });
