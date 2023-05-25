c333f7d409c5e6ed0a07770075c2505f18b35b4d
gizeasy <gizeasy@gmail.com>
2023-05-25 15:06:39 +0300

    docs(standConfig): add figmaUrl

--- src/stand/standConfig.ts
+++ src/stand/standConfig.ts
@@ -22,4 +22,6 @@ export const { createStand } = createConfig({
   image,
   description: 'Библиотека компонентов, из которых собирается шапка проекта.',
   repositoryUrl: 'https://github.com/consta-design-system/header',
+  figmaUrl:
+    'https://www.figma.com/file/LKYyjgQ3khFHvXraZAG1n1/Consta-Components?type=design&node-id=1127-48741&t=SVltkNKB1jSuFlbr-0',
 });
